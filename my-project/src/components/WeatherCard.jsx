65c95975b46d06196f53415cfeeba596dcc269c0
Jonah <okirujonah@gmail.com>
2025-03-17 13:21:07 +0300

    Add comments to  WeatherCard component

--- my-project/src/components/WeatherCard.jsx
+++ my-project/src/components/WeatherCard.jsx
@@ -6,18 +6,23 @@ const WeatherCard = ({ data }) => {
   return (
     
     <div className="bg-white p-6 rounded-lg shadow-lg mt-4">
-      {/*  */}
+      {/*The card header displaying the name of the city and country code  */}
       <h2 className="text-2xl font-bold">{data.name}, {data.sys.country}</h2>
+      {/* The Temperature of the city */}
       <p className="text-lg">Temperature: {data.main.temp}°C</p>
+      {/* The humidity of the city */}
       <p className="text-lg">Humidity: {data.main.humidity}%</p>
+      {/* The wind speed of the city */}
       <p className="text-lg">Wind Speed: {data.wind.speed} km/h</p>
+      {/* weather Condition icon */}
       <img
          src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
          alt={data.weather[0].description}
       />
+      {/* Description of the weather condition */}
       <p className="text-lg capitalize">{data.weather[0].description}</p>
     </div>
   );
 };
 
-export default WeatherCard;
+export default WeatherCard; // Export the Weathercard component.
